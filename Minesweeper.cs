0ab08921739e93027d78baa38dc39ac616a2657d
Henrijs Kravals <henrijskravals98@gmail.com>
2024-01-22 22:04:16 +0200

    Finished Minesweeper in Console

--- Minesweeper.cs
+++ Minesweeper.cs
@@ -4,114 +4,212 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System;
+
 namespace DataStructures
 {
-    // 1. Uzģenērēt 10x10 masīvu ar 10% iespēju, ka konkrētajā cell ir mīna
-    // 2. 
     public class Minesweeper
     {
         private int[][] Grid { get; set; }
         private int[][] MineGrid { get; set; }
-        private int Size { get; set; } // 10
+        private bool[][] Revealed { get; set; }
+        public int Size { get; set; }
 
-        public Minesweeper(int size = 10)
+        public Minesweeper(int size = 4)
         {
             Size = size;
             Grid = new int[size][];
             MineGrid = new int[size][];
-            PopulateGrids(); //Aizpildam režģi
-            PopulateMines();
-            GetMineGrid();
+            Revealed = new bool[size][];
+
+            PopulateLevel();
+            PrintGameBoard();
         }
 
-        private void PopulateGrids() //Šeit mēs aizpildam masīvus ar nullītēm
-        {
-            for (int i = 0; i < Size; i++)
-            {
-                Grid[i] = new int[Size]; //Rinda, kurā mēs izveidojam jauno masīvu
-                MineGrid[i] = new int[Size];
-                for (int j = 0; j < Size; j++)
-                {
-                    Grid[i][j] = 0;
-                    MineGrid[i][j] = 0;
-                }
-            }
-        }
 
-        private void PopulateMines()
-        //0 drošība
-        //1 mīna
+        private void PopulateLevel()
         {
             var random = new Random();
             for (int i = 0; i < Size; i++)
             {
+                Grid[i] = new int[Size];
+                MineGrid[i] = new int[Size];
+                Revealed[i] = new bool[Size];
+
                 for (int j = 0; j < Size; j++)
                 {
+                    Grid[i][j] = 0;
+                    Revealed[i][j] = false;
+
                     var chance = random.Next(10);
                     if (chance == 0)
                     {
                         MineGrid[i][j] = 1;
-
-                        PopulateDetectors(i, j);
+                    }
+                    else
+                    {
+                        MineGrid[i][j] = 0;
                     }
                 }
             }
-        }
 
-        private void PopulateDetectors(int i, int j)
-        {
-            // Check boundaries before incrementing
-            // Vertikāli
-            if (i - 1 >= 0 && i - 1 < Size)
-                Grid[i - 1][j]++;
-            if (i + 1 >= 0 && i + 1 < Size)
-                Grid[i + 1][j]++;
-
-            // Horizontāli
-            if (j - 1 >= 0 && j - 1 < Size)
-                Grid[i][j - 1]++;
-            if (j + 1 >= 0 && j + 1 < Size)
-                Grid[i][j + 1]++;
-
-            // Kreisie stūri
-            if (i - 1 >= 0 && i - 1 < Size && j - 1 >= 0 && j - 1 < Size)
-                Grid[i - 1][j - 1]++;
-            if (i + 1 >= 0 && i + 1 < Size && j - 1 >= 0 && j - 1 < Size)
-                Grid[i + 1][j - 1]++;
-
-            // Labie stūri
-            if (i - 1 >= 0 && i - 1 < Size && j + 1 >= 0 && j + 1 < Size)
-                Grid[i - 1][j + 1]++;
-            if (i + 1 >= 0 && i + 1 < Size && j + 1 >= 0 && j + 1 < Size)
-                Grid[i + 1][j + 1]++;
-        }
-
-        public void GetMineGrid()
-        {   // 5x5
-            // Size = 5
-            // [  j  0  1  2  3  4
-            //  i
-            //  0: [ 0, 0, 0, 0, 0]
-            //  1: [ 0, 0, 0, 0, X]
-            //  2: [ 0, X, 0, 0, 0]
-            //  3: [ 0, 0, 0, X, 0]
-            //  4: [ 0, 0, 0, 0, 0]
-            // ]
+            // Detektorus pēc tam 
             for (int i = 0; i < Size; i++)
             {
-                Console.WriteLine();
                 for (int j = 0; j < Size; j++)
                 {
                     if (MineGrid[i][j] == 1)
                     {
-                        Console.Write("X ");
+                        PopulateDetectors(i, j);
+                    }
+                }
+            }
+
+            // Add debug information
+            Console.WriteLine("Līmenis izveidots, spēle sākas.");
+            PrintGrid();
+
+        }
+
+
+        private void PopulateDetectors(int i, int j)
+        {
+            // V
+            if (i - 1 >= 0)
+            {
+                Grid[i - 1][j]++;
+                if (j - 1 >= 0)
+                    Grid[i - 1][j - 1]++;
+                if (j + 1 < Size)
+                    Grid[i - 1][j + 1]++;
+            }
+
+            if (i + 1 < Size)
+            {
+                Grid[i + 1][j]++;
+                if (j - 1 >= 0)
+                    Grid[i + 1][j - 1]++;
+                if (j + 1 < Size)
+                    Grid[i + 1][j + 1]++;
+            }
+
+            // H
+            if (j - 1 >= 0)
+                Grid[i][j - 1]++;
+            if (j + 1 < Size)
+                Grid[i][j + 1]++;
+        }
+
+        // Atblusošanai (for debug)
+        private void PrintGrid()
+        {
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    if (MineGrid[i][j]==1)
+                    { 
+                        Console.Write("X "); 
                     }
                     else
                     {
                         Console.Write(Grid[i][j] + " ");
                     }
+                    
+                    
+                }
+                Console.WriteLine();
+            }
+        }
+        //
+
+        public void PrintGameBoard()
+        {
+            for (int i = 0; i < Size; i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < Size; j++)
+                {
+                    if (Revealed[i][j])
+                    {
+                        if (MineGrid[i][j] == 1)
+                            Console.Write("X ");
+                        else
+                            Console.Write(Grid[i][j] + " ");
+                    }
+                    else
+                    {
+                        Console.Write("_ ");
+                    }
                 }
             }
         }
+
+        public bool Play(int row, int col)
+        {
+            if (MineGrid[row][col] == 1)
+            {
+                Console.WriteLine("Spēles beigas! Tu uzdūries mīnai!");
+                Revealed[row][col] = true;
+                return true;
+            }
+            else
+            {
+                RevealCell(row, col);
+
+                if (AllNonMineCellsRevealed())
+                {
+                    Console.WriteLine("Tu uzvarēji!");
+                    return true;
+                }
+
+                return false; // Spēle turpinās
+            }
+        }
+
+        private bool AllNonMineCellsRevealed()
+        {
+            for (int i = 0; i < Size; i++)
+            {
+                for (int j = 0; j < Size; j++)
+                {
+                    if (MineGrid[i][j] == 0 && !Revealed[i][j])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        private void RevealCell(int row, int col)
+        {
+            Revealed[row][col] = true;
+
+            
+        }
+    }
+
+    public class UserInput
+    {
+        public static bool TryGetCellInput(out int row, out int col, int size)
+        {
+            Console.WriteLine();
+            Console.Write("Ievadi rindu (0 līdz {0}): ", size - 1);
+            if (int.TryParse(Console.ReadLine(), out row) && row >= 0 && row < size)
+            {
+                Console.Write("Ievadi kolonnu (0 līdz {0}): ", size - 1);
+                if (int.TryParse(Console.ReadLine(), out col) && col >= 0 && col < size)
+                {
+                    return true;
+                }
+            }
+
+            Console.WriteLine("Lūdzu ievadīt derīgus skaitļus.");
+            row = col = -1;
+            return false;
+        }
     }
 }
+
+
--- Program.cs
+++ Program.cs
@@ -1,16 +1,52 @@
 ﻿
+using static DataStructures.Minesweeper;
+
 namespace DataStructures
 {
     internal class Program
     {
+
+
+        /********* For Minesweeper *********/
+
+        static void Main()
+        {
+            PlayGame();
+        }
+
+        static void PlayGame()
+        {
+            Minesweeper minesweeper = new Minesweeper();
+
+            while (true)
+            {
+                int row, col;
+
+                // Get user input until a valid input is provided
+                while (!UserInput.TryGetCellInput(out row, out col, minesweeper.Size)) { }
+
+                // Make a move in the game and check if it's over or won
+                bool gameOverOrWon = minesweeper.Play(row, col);
+
+                // Print the updated game board
+                minesweeper.PrintGameBoard();
+
+                // Check if the game is over or won
+                if (gameOverOrWon)
+                {
+                    break;
+                }
+            }
+        }
+
+
+
+
+
+        /******* For ReturnPaleandrome
+             
         private static void Main(string[] args)
         {
-            var minesweeper = new Minesweeper();
-            
-            
-
-
-            /******* For ReturnPaleandrome
             while (true)
             {
                 Console.Write("Enter a string (or 'exit' to quit): ");
@@ -23,9 +59,9 @@ namespace DataStructures
 
                 ReturnPalendrome.PrintPalindrome(input);
             }
+        }
             *********/
 
-        }
     }
 }
 
